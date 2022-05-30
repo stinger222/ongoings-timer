ce553b93b433d2fe96ced689cc26e85e186a56b2
= <azazasdfg.com@gmail.com>
2022-05-31 00:44:04 +0300

    get rid of "concatClassNames" utility

--- src/components/HeaderButton.tsx
+++ src/components/HeaderButton.tsx
@@ -1,6 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
 import { selectDay } from "../redux/reducers/headerReducer";
-import concatClassNames from "../utils/concatClassNames";
 import styles from './Header/Header.module.css';
 
 export default function HeaderButton({id, children}:any) {
@@ -8,17 +7,17 @@ export default function HeaderButton({id, children}:any) {
 	const { selectedDay, today} = useSelector((state:any) => state.headerReducer)
 	const dispatch = useDispatch()
 
-	const isSelectedClass = selectedDay == id ? styles.is_selected : ''
-	const isTodayClass = today == id ? styles.is_today : ''
-	const _className = concatClassNames(isSelectedClass, isTodayClass)
+	const classNames = `
+		${selectedDay == id ? styles.is_selected : ''} 
+		${today == id ? styles.is_today : ''}
+	`
 
 	const handleClick = () => {
 		dispatch(selectDay(id))
 	}
 	
-
 	return (
-		<button onClick={handleClick} className={_className}>
+		<button onClick={handleClick} className={classNames}>
 			{children}
 		</button>
 	)

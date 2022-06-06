8d5d1b0e9794c56aaf11539a9a481c71b5bd10fc
= <azazasdfg.com@gmail.com>
2022-06-06 19:05:44 +0300

    add message for empty days

---
+++ src/components/EmptyDay/EmptyDay.tsx
@@ -0,0 +1,9 @@
+import styles from './EmptyDay.module.css'
+import img from "../../images/204_1.png"
+
+
+export default function EmptyDay() {
+	return (
+		<img className={styles.img_204} src={img}/>
+	)
+}
--- src/components/pages/TrackPage/TrackPage.tsx
+++ src/components/pages/TrackPage/TrackPage.tsx
@@ -1,15 +1,28 @@
-import { useAppSelector } from "../../../hooks/redux"
-import TrackItem from "../../TrackItem/TrackItem"
-import styles from "./TrackPage.module.css"
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../hooks/redux";
+import { fetchCardsData } from "../../../redux/reducers/cardsReducer";
+import EmptyDay from "../../EmptyDay/EmptyDay";
+import TrackItem from "../../TrackItem/TrackItem";
+import styles from "./TrackPage.module.css";
 
 export default function TrackPage() {
+	const dispatch = useAppDispatch()
 	const selectedDay = useAppSelector(state => state.headerReducer.selectedDay)
 	const selectedDayCardsData = useAppSelector(state => state.cardsReducer.distributedData[selectedDay])
 
+	//temp
+	const fetchCards = () => {
+		dispatch(fetchCardsData())
+	}
+
 	return (
 		<section className={`${styles.track_list} container`}>
-			{selectedDayCardsData && 
-				<TrackItem {...selectedDayCardsData}/>
+			<button onClick={fetchCards}>adslfksdlfk</button>
+			{
+				selectedDayCardsData?.length !== 0 && selectedDayCardsData.map(data => <TrackItem {...data} key={data.cardId}/>)
+			}
+			{
+				selectedDayCardsData?.length === 0 && <EmptyDay/> 
 			}
 		</section>
 	)

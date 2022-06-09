05a4eff81733c05baead9e1e2eff3f78ca1c6720
= <azazasdfg.com@gmail.com>
2022-06-09 22:04:14 +0300

    add opacity transition for EmptyDay component

--- src/components/EmptyDay/EmptyDay.tsx
+++ src/components/EmptyDay/EmptyDay.tsx
@@ -1,9 +1,26 @@
 import styles from './EmptyDay.module.css'
 import img from "../../images/204_1.png"
-
+import { animated, useSpring } from "react-spring"
 
 export default function EmptyDay() {
+
+	const spring = useSpring({
+		from: {
+			opacity: 0,
+		},
+		to: {
+			opacity: 1,
+		},
+		delay: 50,
+		config: {
+			duration: 250
+		}
+	})
+
 	return (
-		<img className={styles.img_204} src={img}/>
+		<animated.div style={spring}>
+			<h1 className={styles.message}>Nothing Here Yet</h1>
+			<img  className={styles.img_204} src={img}/>
+		</animated.div>
 	)
 }

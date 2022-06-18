ded9b1834266d71062f864e3dfc14de0419c41d2
= <azazasdfg.com@gmail.com>
2022-06-19 00:25:17 +0300

    paint hedaer buttons in gray if day is empty

--- src/components/HeaderButton.tsx
+++ src/components/HeaderButton.tsx
@@ -6,16 +6,17 @@ import styles from './Header/Header.module.css';
 interface IHeaderButtonProps {
 	id: number,
 	children?: React.ReactNode,
-} 
+}
 
 export default function HeaderButton({id, children}: IHeaderButtonProps) {
-
-	const { selectedDay, today} = useAppSelector(state => state.headerReducer)
 	const dispatch = useAppDispatch()
+	const { selectedDay, today} = useAppSelector(state => state.headerReducer)
+	const isDayEmpty = useAppSelector(state => state.cardsReducer.distributedData[id].length === 0) 
 
 	const classNames = `
 		${selectedDay == id ? styles.is_selected : ''} 
 		${today == id ? styles.is_today : ''}
+		${isDayEmpty ? styles.is_empty : ''}
 	`
 
 	const handleClick = () => {

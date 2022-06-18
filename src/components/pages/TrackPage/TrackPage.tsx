18874c7f485e548b44763a44e6f0df2892b7036a
= <azazasdfg.com@gmail.com>
2022-06-19 00:31:52 +0300

    show loader while request is in "pending" state

--- src/components/pages/TrackPage/TrackPage.tsx
+++ src/components/pages/TrackPage/TrackPage.tsx
@@ -3,11 +3,14 @@ import { fetchCardsData } from "../../../redux/reducers/cardsReducer";
 import TrackItem from "../../TrackItem/TrackItem";
 import EmptyDay from "../../EmptyDay/EmptyDay";
 import styles from "./TrackPage.module.css";
+import Loader from "../../Loader/Loader";
 
 export default function TrackPage() {
 	const dispatch = useAppDispatch()
 	const selectedDay = useAppSelector(state => state.headerReducer.selectedDay)
 	const selectedDayCardsData = useAppSelector(state => state.cardsReducer.distributedData[selectedDay])
+	const isPending = useAppSelector(state => state.cardsReducer.isPending)
+	
 
 	//temp
 	const fetchCards = () => {
@@ -16,15 +19,23 @@ export default function TrackPage() {
 
 	return (
 		<section className={`${styles.track_list} container`}>
-			<button onClick={fetchCards}>adslfksdlfk</button>
+			<button style={{border: "1px solid black", borderRadius: "10px", fontSize: "1.5em", marginBottom: "10px"}}
+			onClick={fetchCards}>adslfksdlfk</button>
 			{
+				// Render cards
 				selectedDayCardsData?.length !== 0 && selectedDayCardsData.map((data, index) => (
 					<TrackItem cardData={data} index={index} key={data.cardId}/>)
 				)
 			}
-			
+
 			{
-				selectedDayCardsData?.length === 0 && <EmptyDay/> 
+				// Render loader
+				isPending && <Loader/>
+			}
+
+			{
+				// Render stub if there is no cards 
+				selectedDayCardsData?.length === 0 && !isPending && <EmptyDay/> 
 			}
 		</section>
 	)

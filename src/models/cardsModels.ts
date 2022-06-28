befaa4e42ecf70d54401c73b98b871d9527a894e
= <azazasdfg.com@gmail.com>
2022-06-28 20:42:18 +0300

    add init state for dev version of the app

--- src/models/cardsModels.ts
+++ src/models/cardsModels.ts
@@ -8,3 +8,91 @@ export interface ITrelloCardData {
 	cardUrl: string,
 	cardId: string,
 }
+
+export const DEV_destributedData = [
+	[
+		{
+			checkItems: 99,
+			checkItemsChecked: 80,
+			checklistId: "sldfsdfkfjs#d(*98342",
+			cardTitle: "TEST CARD - Вс 00:09",
+			cardDesc: `https://amedia.online/1086-perestanu-byt-geroem.html\nhttps://amedia.online/uploads/posts/2022-03/thumbs/1648116177_1647531170_2.jpg`,
+			cardUrl: "Trello card url here",
+			cardId: "sdsdflkfjsdf29ы0dsdfsflkj234",
+			cardDayId: 0
+		}
+	], [
+		{
+			checkItems: 99,
+			checkItemsChecked: 80,
+			checklistId: "sldfsdfkfjs#d(*98342",
+			cardTitle: "TEST CARD - Пн 12:09",
+			cardDesc: `https://amedia.online/1086-perestanu-byt-geroem.html\nhttps://amedia.online/uploads/posts/2022-03/thumbs/1648116177_1647531170_2.jpg`,
+			cardUrl: "Trello card url here",
+			cardId: "sdlkfjвsdf290dsавflkj234",
+			cardDayId: 1
+		}
+	], [
+		{
+			checkItems: 99,
+			checkItemsChecked: 80,
+			checklistId: "sldfsdfkfjs#d(*98342",
+			cardTitle: "TEST CARD - Вт 12:09",
+			cardDesc: `https://amedia.online/1086-perestanu-byt-geroem.html\nhttps://amedia.online/uploads/posts/2022-03/thumbs/1648116177_1647531170_2.jpg`,
+			cardUrl: "Trello card url here",
+			cardId: "sdlkывааfjsdf29а0dssdfflkj234",
+			cardDayId: 2
+		}
+	], [
+		{
+			checkItems: 99,
+			checkItemsChecked: 80,
+			checklistId: "sldfsdfkfjs#d(*98342",
+			cardTitle: "TEST CARD - Ср 12:09",
+			cardDesc: `https://amedia.online/1086-perestanu-byt-geroem.html\nhttps://amedia.online/uploads/posts/2022-03/thumbs/1648116177_1647531170_2.jpg`,
+			cardUrl: "Trello card url here",
+			cardId: "sdlkfjвsdf29а0dsdfsflkj234",
+			cardDayId: 3
+		}
+		
+	], [{
+		checkItems: 99,
+		checkItemsChecked: 80,
+		checklistId: "sldfsdfkfjs#d(*98342",
+		cardTitle: "TEST CARD - Чт 12:09",
+		cardDesc: `https://amedia.online/1086-perestanu-byt-geroem.html\nhttps://amedia.online/uploads/posts/2022-03/thumbs/1648116177_1647531170_2.jpg`,
+		cardUrl: "Trello card url here",
+		cardId: "sdlksdffjыsdfва290dsflkj234",
+		cardDayId: 4
+	}], [{
+		checkItems: 99,
+		checkItemsChecked: 80,
+		checklistId: "sldfsdfkfjs#d(*98342",
+		cardTitle: "TEST CARD - Пт 12:09",
+		cardDesc: `https://amedia.online/1086-perestanu-byt-geroem.html\nhttps://amedia.online/uploads/posts/2022-03/thumbs/1648116177_1647531170_2.jpg`,
+		cardUrl: "Trello card url here",
+		cardId: "sdlkfjsdf29вав0dssdfsdfflkj234",
+		cardDayId: 5
+	}], 
+	[{
+		checkItems: 99,
+		checkItemsChecked: 80,
+		checklistId: "sldfsdfkfjs#d(*98342",
+		cardTitle: "TEST CARD - Сб 12:09",
+		cardDesc: `https://amedia.online/1086-perestanu-byt-geroem.html\nhttps://amedia.online/uploads/posts/2022-03/thumbs/1648116177_1647531170_2.jpg`,
+		cardUrl: "Trello card url here",
+		cardId: "sdlkfjsdf2вав9sdf0dsflkj234",
+		cardDayId: 6
+	},
+	{
+		checkItems: 99,
+		checkItemsChecked: 80,
+		checklistId: "sldfsdfkfjs#d(*98342",
+		cardTitle: "TEST CARD - Сб 20:00",
+		cardDesc: `https://amedia.online/1086-perestanu-byt-geroem.html\nhttps://amedia.online/uploads/posts/2022-03/thumbs/1648116177_1647531170_2.jpg`,
+		cardUrl: "Trello card url here",
+		cardId: "sdlkfjsdf2вавsdf90dsflkj234",
+		cardDayId: 6
+	}
+]
+]

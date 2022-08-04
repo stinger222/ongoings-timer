69b498bd18c7675f2b607ec5f1d373fa653f0287
= <azazasdfg.com@gmail.com>
2022-08-04 14:28:24 +0300

    add a little validation to "AddCard" form
    closes #31

---
+++ src/components/Dropdown/AddCardForm.tsx
@@ -0,0 +1,104 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import styles from './Dropdown.module.css';
+
+export default function AddCardForm({ handleSubmit }: any) {
+	return (
+		<Formik
+			initialValues={{
+				title: "",
+				day: "Пн",
+				time: "00:00",
+				length: "",
+				watched: "",
+				player_url: "",
+				thumbnail_url: "",
+			}}
+			onSubmit={(values, { resetForm }) => {
+				handleSubmit(values)
+				resetForm()
+			}}
+			validate={(values) => {
+				const errors: any = {};
+
+				const length = +values.length.trim()
+				const watched = +values.watched.trim()
+
+				if (isNaN(length)) {
+					errors["length"] = true
+				}
+
+				if (isNaN(watched)) {
+					errors["watched"] = true
+				}
+
+				if (errors) return errors
+			}}
+		>
+			<Form className={styles.body}>
+				<div className={styles.dropdown_row}>
+					<ErrorMessage name="title" >
+						{msg => <span className={styles.error_message}></span>}
+					</ErrorMessage>
+					<Field
+						required
+						className={styles.dropdown_input}
+						name="title"
+						placeholder="Card Title"
+					/>
+				</div>
+
+				<div className={styles.dropdown_row}>
+					<Field as="select" name="day">
+						<option value="Пн">Пн</option>
+						<option value="Вт">Вт</option>
+						<option value="Ср">Ср</option>
+						<option value="Чт">Чт</option>
+						<option value="Пт">Пт</option>
+						<option value="Сб">Сб</option>
+						<option value="Вс">Вс</option>
+					</Field>
+					<Field className={styles.dropdown_input} name="time" type="time" />
+				</div>
+
+				<div className={styles.dropdown_row}>
+					<ErrorMessage name="length" >
+						{msg => <span className={styles.error_message}></span>}
+					</ErrorMessage>
+					<Field
+						className={styles.dropdown_input}
+						name="length"
+						placeholder="Length"
+					/>
+					<ErrorMessage name="watched" >
+						{msg => <span className={styles.error_message}></span>}
+					</ErrorMessage>
+					<Field
+						className={styles.dropdown_input}
+						name="watched"
+						placeholder="Watched"
+					/>
+				</div>
+
+				<div className={styles.dropdown_row}>
+					<Field
+						className={styles.dropdown_input}
+						name="player_url"
+						placeholder="Player url"
+					/>
+				</div>
+
+				<div className={styles.dropdown_row}>
+					<Field
+						className={styles.dropdown_input}
+						name="thumbnail_url"
+						placeholder="Thumbnail url"
+					/>
+				</div>
+
+				<button className={styles.dropdown_button} type="submit">
+					Create
+				</button>
+			</Form>
+		</Formik>
+	);
+}
--- src/components/Dropdown/DropdownAddCard.tsx
+++ src/components/Dropdown/DropdownAddCard.tsx
@@ -1,15 +1,18 @@
 import { useAppDispatch } from "../../hooks/redux";
+import { cretaeCard } from "../../redux/reducers/cardsReducer";
 import { setActiveMenu } from "../../redux/reducers/headerReducer";
+import { INewCardData, storageKeys } from "../../models/trelloModels";
+
+import { processTitle } from "../../utils/hepler";
+
 import { ReactComponent as BackIcon } from "../../assets/back.svg";
 import styles from './Dropdown.module.css';
-import { INewCardData, storageKeys } from "../../models/trelloModels";
-import { cretaeCard } from "../../redux/reducers/cardsReducer";
-import { processTitle } from "../../utils/hepler";
+import AddCardForm from "./AddCardForm";
 
 const getNewCardData = ({
   title, day, time, length, watched, player_url, thumbnail_url
 }: any): INewCardData => {
-  const name = `${title} - ${day} ${time}`
+  const name = `${processTitle(title)} - ${day} ${time}`
   const desc = player_url + "\n" + thumbnail_url
   const selectedList = localStorage.getItem(storageKeys.selectedList)
 
@@ -27,28 +30,8 @@ const getNewCardData = ({
 export default function DropdownAddCard({ className }: any) {
   const dispatch = useAppDispatch()
 
-  const handleCardCreation = (e: any) => {
-    e.preventDefault()
-    const formData = {
-      title: processTitle(e.target.title.value),
-      day: e.target.day.value,
-      time: e.target.time.value,
-      length: +e.target.length.value,
-      watched: +e.target.watched.value,
-      player_url: e.target.player_url.value,
-      thumbnail_url: e.target.thumbnail_url.value
-    }
-    
-    e.target.title.value = ''
-    e.target.time.value = '00:00'
-    e.target.length.value = ''
-    e.target.watched.value = ''
-    e.target.player_url.value = ''
-    e.target.thumbnail_url.value = ''
-    
-
-    const newCardData: INewCardData = getNewCardData(formData)
-    
+  const handleSubmit = (formValues: any) => {
+    const newCardData: INewCardData = getNewCardData(formValues)
     dispatch(cretaeCard(newCardData))
   }
 
@@ -61,40 +44,6 @@ export default function DropdownAddCard({ className }: any) {
       Add New Card  
     </h1>
 
-		<form className={styles.body} onSubmit={handleCardCreation}> 
-      <div className={styles.dropdown_row}>
-        <input className={styles.dropdown_input} id="title" placeholder="Card Title"></input>
-      </div>
-
-      <div className={styles.dropdown_row}>
-        <select id="day">
-          <option>Пн</option>
-          <option>Вт</option>
-          <option>Ср</option>
-          <option>Чт</option>
-          <option>Пт</option>
-          <option>Сб</option>
-          <option>Вс</option>
-        </select>
-        <input className={styles.dropdown_input} id="time" type="time" defaultValue="00:00"></input>
-      </div>
-
-      <div className={styles.dropdown_row}>
-        <input className={styles.dropdown_input} id="length" placeholder="Length"></input>
-        <input className={styles.dropdown_input} id="watched" placeholder="Watched"></input>
-      </div>
-
-      <div className={styles.dropdown_row}>
-        <input className={styles.dropdown_input} id="player_url" placeholder="Player url"></input>
-      </div>
-
-      <div className={styles.dropdown_row}>
-        <input className={styles.dropdown_input} id="thumbnail_url" placeholder="Thumbnail url"></input>
-      </div>
-
-      <button className={styles.dropdown_button} type="submit">
-        Create
-      </button>
-		</form>
+		<AddCardForm handleSubmit={handleSubmit}/>
   </div>
 }

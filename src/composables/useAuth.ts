c8acf5938b7e9c0cb8b44c027bb83e44896d0f85
Pavel <azazasdfg.com@gmail.com>
2025-10-25 14:46:26 +0300

    feat: Make auth persistent across reloads

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -1,11 +1,17 @@
 import { auth } from "@/services/firebase"
-import { GoogleAuthProvider, signInWithPopup, type User } from "firebase/auth"
+import { GoogleAuthProvider, signInWithPopup, type User, onAuthStateChanged, setPersistence, browserLocalPersistence } from "firebase/auth"
 import { ref } from "vue"
 
 export const useAuth = () => {
   const googleProvider = new GoogleAuthProvider()
   const user = ref<User | null>(null)
 
+  setPersistence(auth, browserLocalPersistence)
+  
+  onAuthStateChanged(auth, (u) => {
+    user.value = u
+  })
+  
   const signIn = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider)
